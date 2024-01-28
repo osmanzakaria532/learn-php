ea28eaf1e76930bdf7af6bf96529f5181eb9e20c
osmanzakaria532 <osmanzakaria179@gmail.com>
2024-01-28 22:54:40 +0600

    php 02

---
+++ php_02/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+        $name = "Osman Zakaria";
+        $phone_number = "01686942329";
+        $salary = 12000;
+        $house_rant = 5000;
+
+        echo "Osman Zakaria $phone_number text <br>"; 
+
+        echo "Osman Zakaria ".$phone_number." <br> porer text ";
+
+        // echo "Osman Zakaria ".$salary + $house_rant;
+        // echo "Osman Zakaria $salary" + $house_rant; ( Unsupported operand types: string + int )
+
+    ?>
+    
+</body>
+</html>
