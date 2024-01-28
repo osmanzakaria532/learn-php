0fb3a3111124c379f2bc7272d6b2d7edf1cfe10e
osmanzakaria532 <osmanzakaria179@gmail.com>
2024-01-28 23:09:31 +0600

    php 03 > local, global & static Scope

---
+++ php_03/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php
+        // local, global, static
+
+        // function  => Built In Function, User Define Function
+
+        /*
+        function
+        function_name
+        () header
+        {} body
+        */
+
+        function getAddress(){
+            echo "Dhaka";
+        }
+
+        getAddress();
+
+
+    ?>
+
+</body>
+</html>
