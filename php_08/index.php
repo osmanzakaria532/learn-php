95c11b0a3c26f76d84a071474587c1ea772a5efc
osmanzakaria532 <osmanzakaria179@gmail.com>
2024-01-29 22:26:04 +0600

    data type in PHP

---
+++ php_08/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+>> Data Types in PHP
+1. string; ( "" , '' )
+2. integer; (one digit, no decimal, negative ba positive)
+3. float (double);
+4. boolean; ( true > ( value 1 ) ot false > ( value 0 ) to use conditional function )
+5. array;
+6. object;
+7. null;
+8. resource;
+
+
+find out with "var_dump" data type of any dataType in php 
+var_dump() is a building function to return data type and value
+*/
+
+$arr_type = array("OsmanZakaria", "Chattagram");
+
+var_dump($arr_type);
