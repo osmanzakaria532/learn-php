8043c9e1cac14752a3d9f09d12475368ed142b99
osmanzakaria532 <osmanzakaria179@gmail.com>
2024-01-29 21:44:02 +0600

    different between echo and print

--- php_07/index.php
+++ php_07/index.php
@@ -5,3 +5,20 @@
 // echo > one parameter & slow loading
 
 echo "Osman ", "Zakaria ", "01686942329";
+
+echo "<br>";
+
+$qty = 5;
+$price = 10;
+$product_name = "Cat";
+
+echo $qty.$price."<br>";
+
+echo $product_name;
+
+echo "<br>";
+
+print $product_name;
+echo "<br>";
+
+print($product_name) ;
